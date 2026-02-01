1381e8ea6acd2cb0303e3049a58f451701f3b87b
justinroche <justinroche03@gmail.com>
2026-02-01 17:52:27 -0600

    Set up docker for front end

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {
+  client_host,
+  client_port,
+} from './src/config/config.json'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: client_host,
+    port: client_port,
+  },
 })

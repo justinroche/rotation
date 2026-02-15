15977dacd7f6b767e83bf3eaf01ee86015da2904
justinroche <justinroche03@gmail.com>
2026-02-15 17:51:00 -0600

    Simplify address config

--- server/src/config.rs
+++ server/src/config.rs
@@ -12,12 +12,14 @@ static CONFIG: OnceCell<Config> = OnceCell::new();
 pub fn init() -> &'static Config {
     dotenvy::dotenv().ok();
 
-    let host = env::var("SERVER_HOST").expect("SERVER_HOST environment variable is required");
-    let port = env::var("SERVER_PORT").expect("SERVER_PORT environment variable is required");
-    let addr: SocketAddr =
-        format!("{}:{}", host, port).parse().expect("SERVER_HOST or SERVER_PORT is invalid");
+    let addr: SocketAddr = env::var("BIND_ADDR")
+        .expect("BIND_ADDR environment variable is missing")
+        .parse()
+        .expect("BIND_ADDR is invalid");
 
-    CONFIG.set(Config { addr }).expect("Config already initialized");
+    CONFIG
+        .set(Config { addr })
+        .expect("Config already initialized");
 
     CONFIG.get().unwrap()
 }
@@ -25,5 +27,7 @@ pub fn init() -> &'static Config {
 #[allow(dead_code)]
 /// Get a reference to the global config. Panics if `init()` wasn't called.
 pub fn get() -> &'static Config {
-    CONFIG.get().expect("Config not initialized. Call config::init() in main.")
+    CONFIG
+        .get()
+        .expect("Config not initialized. Call config::init() in main.")
 }

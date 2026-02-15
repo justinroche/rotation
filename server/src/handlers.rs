0da73c4249914350adccbd5a672fdc487ab8e045
justinroche <justinroche03@gmail.com>
2026-02-15 17:50:43 -0600

    Organize health check - first endpoint!

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -1,10 +1,8 @@
 use axum::{Json, response::IntoResponse};
 use serde_json::json;
 
-pub async fn hello_world() -> impl IntoResponse {
-    let json_response = json!({
-        "status": "ok",
-        "message": "Hello world"
-    });
-    Json(json_response)
+pub async fn health_check() -> impl IntoResponse {
+    Json(json!({
+        "status": "ok"
+    }))
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,12 @@
-use axum::{Router, routing::get};
-
 mod config;
 mod handlers;
-
-use handlers::hello_world;
+mod router;
 
 #[tokio::main]
 async fn main() {
     let config::Config { addr } = config::init();
 
-    let app = Router::new().route("/api", get(hello_world));
+    let app = router::init();
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
     println!("Server started successfully at {}", addr);
